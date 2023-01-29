43dbc5ccd2cbc517491f51400bbcb15aa1eb4a60
teiteipan06 <y7321543@gmail.com>
2023-01-29 21:00:50 +0900

    snack move

--- game.js
+++ game.js
@@ -1,12 +1,32 @@
+var BLOCK_SIZE = 20
+var BLOCK_COUNT = 20
+
 var gameInterval
+var snack
 
 function gameStart() {
   gameInterval = setInterval(gameRoutine, 1000)
 }
 
 function gameRoutine() {
+  moveSnack()
   updateCanvas()
 }
+
+function moveSnack() {
+  var newBlock = {
+    x: snack.body[0].x + snack.direction.x,
+    y: snack.body[0].y + snack.direction.y
+  }
+
+  snack.body.unshift(newBlock)
+
+  while(snack.body.length > snack.size) {
+    snack.body.pop()
+  }
+}
+
+
 
 function updateCanvas() {
   var canvas = document.getElementById('canvas_id')
